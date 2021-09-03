93b07c9954b4e634c3234bc40f69e2e7f69ab87c
Raymond <70726900+ray165@users.noreply.github.com>
2021-09-02 21:26:30 -0700

    Created getters and setters, but this seems to be redundant with lombok

--- server/src/main/java/landingpage/server/Product.java
+++ server/src/main/java/landingpage/server/Product.java
@@ -7,12 +7,6 @@ import javax.persistence.*;
 import java.math.BigDecimal;  
 
 public class Product {
-    public Product(String name, String description, String code, Float price) {
-        this.name = name;
-        this.description = description;
-        this.code = code;
-        this.price = price;
-    }
 
     @Id
     @GenerateValue(strategy = GenerationType.AUTO)
@@ -25,4 +19,48 @@ public class Product {
     private String code;
 
     private Float price;
+
+    public Product(String name, String description, String code, Float price) {
+        this.name = name;
+        this.description = description;
+        this.code = code;
+        this.price = price;
+    }
+
+
+    // Getters & Setters redundant because of lombok import
+
+    // public String getName() {
+    //     return this.name;
+    // }
+
+    // public String getDescription() {
+    //     return this.description;
+    // }
+
+    // public String getCode() {
+    //     return this.code;
+    // }
+
+    // public Float getPrice() {
+    //     return this.price;
+    // }
+
+    // //Setters
+    // public void setName(String newName) {
+    //     this.name = newName;
+    // }
+
+    // public void setDescription(String newDescription) {
+    //     this.description = newDescription;
+    // }
+
+    // public void setCode(String newCode) {
+    //     this.code = newCode;
+    // }
+
+    // public void setPrice(Float newPrice) {
+    //     this.price = newPrice;
+    // }
+
 }
